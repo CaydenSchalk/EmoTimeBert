bf48cb1a127735a082c7e4dd2193a2941c1b45a8
cayden <schalkcayden@gmail.com>
2026-01-29 18:32:29 -0600

    added data handling folder

---
+++ data_handling/datasets/MELD.py
@@ -0,0 +1,21 @@
+import torch
+from torch.utils.data import Dataset
+
+class MELDDataset(Dataset):
+    def __init__(self, conversations, tokenizer, max_len=128):
+        self.conversations = conversations
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+
+    def __len__(self):
+        return len(self.conversations)
+
+    def __getitem__(self, idx):
+        conv = self.conversations[idx]
+
+        return {
+            "texts": conv["texts"],
+            "labels": torch.tensor(conv["labels"], dtype=torch.long),
+            "timestamps": torch.tensor(conv["timestamps"], dtype=torch.long),
+            "speakers": torch.tensor(conv["speakers"], dtype=torch.long)
+        }
